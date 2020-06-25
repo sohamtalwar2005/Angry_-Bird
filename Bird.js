44c5952db8b7dad45b4c281e8e71217161d78407
sohamtalwar2005 <sohamtalwar2005@gmail.com>
2020-06-25 19:33:43 +0530

    test

--- Bird.js
+++ Bird.js
@@ -11,8 +11,10 @@ class Bird extends BaseClass {
     //this.body.position.y = mouseY;
 
     super.display();
-
-    if(this.body.velocity.x > 10 && this.body.position.x > 200){
+    if(gameState === "reset"){
+      this.trajectory =[]
+    }
+    if(this.body.velocity.x > 10 && this.body.position.x > 200 && gameState!== "reset"){
       var position = [this.body.position.x, this.body.position.y];
       this.trajectory.push(position);
     }
--- sketch.js
+++ sketch.js
@@ -7,11 +7,13 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var score = 0;
 var gameState = "onSling";
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    //backgroundImg = loadImage("sprites/bg.png");
+    getTime();
+
 }
 
 function setup(){
@@ -45,10 +47,15 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg){
     background(backgroundImg);
     Engine.update(engine);
+    fill('white')
+    text(score,1000,50)
     //strokeWeight(4);
     box1.display();
+    pig1.score();
+    pig3.score();
     box2.display();
     ground.display();
     pig1.display();
@@ -67,7 +74,9 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+    }
 }
+
 
 function mouseDragged(){
     if (gameState!=="launched"){
@@ -83,6 +92,22 @@ function mouseReleased(){
 
 function keyPressed(){
     if(keyCode === 32){
-       // slingshot.attach(bird.body);
+        slingshot.attach(bird.body);
+        gameState = "reset"
     }
+}
+ async function getTime(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responseJSON = await response.json();
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    console.log(datetime);
+    if(hour>=06 && hour<=18){
+        bg ="sprites/bg.png";
+    }
+    else{
+        bg = "sprites/bg2.jpg";
+    } 
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
 }
